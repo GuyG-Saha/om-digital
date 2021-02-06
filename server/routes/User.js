8ae21dca69436b6385b4d93e60c0febb08ca0fe2
Guy <guyg7@mail.afeka.ac.il>
2021-02-06 21:20:35 +0200

    Changed /register route and fixed JSON consuming in server

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
-router.get('/register', (req, res) => {
+router.post('/register', (req, res) => {
     try {
-        db.query("INSERT INTO Users (name, password) VALUES ('testuser', 'password')", 
+        const username = req.body.username;
+        const password = req.body.password;
+        db.query("INSERT INTO Users (name, password) VALUES (?, ?)", 
+        [username, password], 
         (err, results) => {
-            console.log(err);
+            if (err)
+                console.log(err);
             res.send(results);
         });
     } catch (Exception) {
-        return console.error('DB error');
+        return console.error(Exception);
     }
 });
 
