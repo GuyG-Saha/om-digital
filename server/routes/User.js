87c7499cf93d624c899128ff10de2f737b1d9c1f
Guy <guyg7@mail.afeka.ac.il>
2021-02-07 09:52:08 +0200

    Initial implementation of login route for users

--- server/routes/User.js
+++ server/routes/User.js
@@ -14,8 +14,31 @@ router.post('/register', (req, res) => {
             res.send(results);
         });
     } catch (Exception) {
-        return console.error(Exception);
+        console.error(Exception);
     }
 });
+
+router.post('/login', (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        db.query('SELECT * FROM Users WHERE name = ?', [username], (err, results) => {
+            if (err)
+                console.log(err)
+            if (results) {
+                // Migrate to BCrypt -- for now dummy style
+                if (results[0].password == password) {
+                    res.send.json({'status': 200, 'message': 'You are logged in!'});
+                } else {
+                    res.send.json({'status': 401, 'message': 'Wrong username or password'});
+                }
+            } else {
+                res.send.json({'status': 404, 'message': 'User does not exist!'})
+            }
+        })
+    } catch (Exception) {
+        console.log(Exception)
+    }
+})
 
 module.exports = router;
